54912dcd7ef3dc3ccd5a4d9cf80b8cab101b0ed8
jyq <jyq52787@163.com>
2015-11-30 05:09:44 +0800

    程序初始化

---
+++ NewsHeadLine/NewsHeadLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsHeadLine
+//
+//  Created by 贾永强 on 15/11/30.
+//  Copyright © 2015年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
